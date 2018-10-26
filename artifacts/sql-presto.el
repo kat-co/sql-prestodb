9f5b51f4c65d5ebcc14ff49de2be7dcb04698c64
Katherine Cox-Buday <cox.katherine.e@gmail.com>
2018-10-26 15:06:04 -0500

    Updated Functions to Match Package Name

--- artifacts/sql-presto.el
+++ artifacts/sql-presto.el
@@ -63,22 +63,22 @@
 ;;; Code:
 (require 'sql)
 
-(defcustom sql-prestodb-program "presto"
+(defcustom sql-presto-program "presto"
   "Command to start the PrestoDB command interpreter."
   :type 'file
   :group 'SQL)
 
-(defcustom sql-prestodb-login-params '(server default-catalog default-schema)
+(defcustom sql-presto-login-params '(server default-catalog default-schema)
   "Parameters needed to connect to PrestoDB."
   :type 'sql-login-params
   :group 'SQL)
 
-(defcustom sql-prestodb-options '("--output-format" "CSV_HEADER")
-  "List of options for `sql-prestodb-program'."
+(defcustom sql-presto-options '("--output-format" "CSV_HEADER")
+  "List of options for `sql-presto-program'."
   :type '(repeat string)
   :group 'SQL)
 
-(defun sql-prestodb-comint (product options &optional buffer-name)
+(defun sql-presto-comint (product options &optional buffer-name)
   "Connect to PrestoDB in a comint buffer.
 
 PRODUCT is the sql product (prestodb). OPTIONS are any additional
@@ -93,7 +93,7 @@ the SQLi buffer to be named."
     (setenv "PRESTO_PAGER" "cat")
     (sql-comint product params buffer-name)))
 
-(defun sql-prestodb (&optional buffer)
+(defun sql-presto (&optional buffer)
   "Run PrestoDB as an inferior process.
 
 The buffer with name BUFFER will be used or created."
@@ -106,11 +106,11 @@ The buffer with name BUFFER will be used or created."
                  :list-table "DESCRIBE %s;"
                  :prompt-regexp "^[^>]*> "
                  :prompt-cont-regexp "^[ ]+-> "
-                 :sqli-comint-func 'sql-prestodb-comint
+                 :sqli-comint-func 'sql-presto-comint
                  :font-lock 'sql-mode-ansi-font-lock-keywords
-                 :sqli-login sql-prestodb-login-params
-                 :sqli-program 'sql-prestodb-program
-                 :sqli-options 'sql-prestodb-options)
+                 :sqli-login sql-presto-login-params
+                 :sqli-program 'sql-presto-program
+                 :sqli-options 'sql-presto-options)
 
 (provide 'sql-presto)
 ;;; sql-presto.el ends here
